81e52f3a3da3cf74dbe4e22be7df358b47556caa
pdrivier <33138648+pdrivier@users.noreply.github.com>
2023-10-16 10:12:13 -0700

    added: visual wf shape comparisons

--- data_wrangling/start_here_set_list_of_interneurons.m
+++ data_wrangling/start_here_set_list_of_interneurons.m
@@ -21,10 +21,14 @@
 %were recorded for that neuron--we don't expect the tissue we've turned
 %away from to be healthy like that, though it's of course possible. 
 
+clear
+close
+dbstop if error
+
 % -------------------------------------------------------------------------
 %We'll first load the original list of interneuron identities--------------
 % -------------------------------------------------------------------------
-load('Int_Identities_Automaze.mat')
+load('Interneuron_Identities_Automaze.mat')
 
 list_to_select_from = Int_Identities;
 delimratsess='D';
@@ -32,8 +36,23 @@ delimtetunit = 'TETSPK';
 min_accept_distance = 10;
 
 unique_cells = select_unique_cells(list_to_select_from,delimratsess,delimtetunit,min_accept_distance);
-clearvars -except unique_cells
-save('unique_ints_by_daytetfrate.mat','unique_cells') %save your output!
+%now, check visually to make sure that avg spike waveforms are in fact
+%distinct across neurons recorded from the same rat and same tetrode, but
+%across different recording days
+plot_avg_spike_waveforms(unique_cells)
+
+rm_inds = [20,10,13];
+%20: LH2D14_TETSPK41d, looks cortical in shape
+%10: LH16D29_TETSPK09a looks very similar in relative amplitudes to 
+%LH16D29_TETSPK09c (the latter has marginally higher firing rate, so keep 
+%that one)
+%13: LH16D35_TETSPK33l looks identical in waveform shape and relative
+%amplitude to LH16D29_TETSPK33i (keep latter bc higher firing rate)
+
+unique_cells(rm_inds,:) = [];
+
+clearvars -except unique_cells Rat
+save('unique_ints_by_daytetfrate.mat','unique_cells','Rat') %save your output!
 
 % -------------------------------------------------------------------------
 %now, run the script that makes your odor sampling intervals
